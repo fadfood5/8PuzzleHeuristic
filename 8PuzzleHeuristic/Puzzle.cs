1d1ff4a8d2cde7d20d321330c3c7c6fd2e541555
fadfood5 <Fadi@USF-Gold-Wifi-226-9-74.laptops.usf.edu>
2017-03-01 11:34:37 -0500

    user input working

--- 8PuzzleHeuristic/Puzzle.cs
+++ 8PuzzleHeuristic/Puzzle.cs
@@ -8,32 +8,73 @@ namespace PuzzleHeuristic{
 	public class Spot{
 		public int x;
 		public int y;
+		public int w;
 
-		public Spot(int a, int b){
+		public Spot(int a, int b, int c){
 			x = a;
 			y = b;
+			w = c;
 		}
 	}
 
 	public class Puzzle{
 		//Instantiate list of nodes and number of vertices
-		Spot[] list;
-		public List<int> nodes;
+		List<Spot> list;
 
 		public Puzzle(int v, int w){
-			nodes = new List<int>();
+
 			//Create number of Spots according to input
-			list = new Spot[v];
+			list = new List<Spot>();
+			/*
 			for (int i = 0; i < v; i++)
 				for (int j = 0; j < w; j++) {
-					list [i] = new Spot (i, j);
+					list.Add(new Spot (i, j));
 					Console.WriteLine ("I: " + i);
 					Console.WriteLine ("J: " + j);
-				}
+				}*/
+		}
+
+		public void addSpot(int i, int j, int wgt){
+
+			//Create Spot Object
+			Spot sp = new Spot(i, j, wgt);
+			//Add spot to Puzzle array
+			list.Add(sp);
+
+			Console.WriteLine("Added edge to vertex: (" + i + ", " + j + ") with weight " + wgt);
 		}
 
 		public static void Main(string[] args){
 			Puzzle p = new Puzzle (3, 3);
+
+			Console.WriteLine ("\nWhat is the name of your .txt file? Make sure it is in the current directory and that there are no whitespace lines. Do not insert the .txt extension.");
+			var b = Console.ReadLine ();
+
+			string line;
+			int counter = 0;
+			System.IO.StreamReader file = new System.IO.StreamReader("../../" + b + ".txt");
+			List<string> lines = new List<string> ();
+			while((line = file.ReadLine()) != null){
+				Console.WriteLine ("Line " + counter + ": " + line);
+				lines.Add (line);
+				counter++;
+			}
+
+			int count1 = 0;
+			int count2 = 0;
+			foreach(string words in lines){
+				string[] w = words.Split(' ');
+				p.addSpot (count1, count2, Int32.Parse(w[0]));
+				count2++;
+				p.addSpot (count1, count2, Int32.Parse(w[1]));
+				count2++;
+				p.addSpot (count1, count2, Int32.Parse(w[2]));
+				count2 = 0;
+				count1++;
+			}
+
+			file.Close();
+
 		}
 	}
 }
